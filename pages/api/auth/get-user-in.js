2dca32bd6be1e7c88293a396fba20c576ddf7780
ABSanthosh <a.b.santhosh02@gmail.com>
2023-02-10 18:57:27 +0530

    fix: attempt to fix login

--- pages/api/auth/get-user-in.js
+++ pages/api/auth/get-user-in.js
@@ -5,12 +5,12 @@ import { getUserIn } from "../../../db/user.db";
 export default withIronSessionApiRoute(getUserInAPI, ironOptions);
 
 async function getUserInAPI(req, res) {
-  try {
+  // try {/
     const user = await getUserIn(req.body);
     req.session.user = user;
     await req.session.save();
     res.json({ status: 200, user });
-  } catch (e) {
-    res.json({ status: 400, message: e });
-  }
+  // } catch (e) {
+  //   res.json({ status: 400, message: e });
+  // }
 }
