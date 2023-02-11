59289d21a7158e1d328ba98101db216f91faca17
ABSanthosh <a.b.santhosh02@gmail.com>
2023-02-11 17:43:52 +0530

    fix: typo

--- pages/api/auth/get-user-in.js
+++ pages/api/auth/get-user-in.js
@@ -6,11 +6,11 @@ export default withIronSessionApiRoute(getUserInAPI, ironOptions);
 
 async function getUserInAPI(req, res) {
   // try {/
-  const addUser = await addUser(req.body);
+  const addedUser = await addUser(req.body);
   const user = await getUserIn(req.body);
   req.session.user = user;
   await req.session.save();
-  res.json({ status: 200, addUser, user });
+  res.json({ status: 200, addedUser, user });
   // } catch (e) {
   //   res.json({ status: 400, message: e });
   // }
